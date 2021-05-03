38159b61c09b1882cb0d2f83a7500566462cf117
Kenny Kvibe <kenny.pizeta@hotmail.com>
2021-05-03 12:12:52 +0200

    Added the CLI argument input before python's input().

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Repository: [py-daily-info](https://github.com/kenny-kvibe/py-daily-info/)
-Python - Script that shows Daily Information
+Python - Script that shows Daily Information of **Horoscope & Numerology**
 
 # Contents:
 - DailyNumber
@@ -11,7 +11,7 @@ Python - Script that shows Daily Information
 ```sh
 git clone https://github.com/kenny-kvibe/py-daily-info DailyInfo
 cd DailyInfo
-python ./main.py "1991-12-31"
+python ./main.py 1991-12-31
 ```
 
 # Example Modules Usage:
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-def main() -> int:
-	from gui_window import Gui
-	from numerology import DailyNumber
-	from horoscope import DailyHoroscope
-
+def main(argv:list[str]) -> int:
 	try:
-		date_input: str = str(input('Enter your date of birth (YYYY-MM-DD): '))
-		default_date: str = '1991-01-31'
-		date = date_input if len(date_input) > 0 else default_date
+		from gui_window import Gui
+		from numerology import DailyNumber
+		from horoscope import DailyHoroscope
+
+		def get_input_date() -> str:
+			input_date: str = str(input('Please enter your date of birth (YYYY-MM-DD): '))
+			return '1991-01-31' if len(input_date) == 0 else input_date
+
+		date: str = get_input_date() if len(argv) <= 1 else str(argv[1])
 
 		txt: str = '\n'
 		txt = txt.join(f'{txt} ~ {repr(i)} ~{txt}{txt}{i}' for i in (DailyHoroscope(date), DailyNumber(date))) + txt
@@ -17,12 +19,14 @@ def main() -> int:
 
 	except:
 		from traceback import format_exc
+
 		print(format_exc())
 		return 1
 
 	return 0
 
+
 if __name__ == '__main__':
-	from sys import exit
-	code:int = main()
+	from sys import exit, argv
+	code: int = main(argv)
 	exit(code)
